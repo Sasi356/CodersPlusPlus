6b9f1cd0dedc0b207febd41c599a94f19970981a
Sasi sekhar <86903824+Sasi356@users.noreply.github.com>
2021-12-07 10:29:27 +0530

    added main.py file

---
+++ main.py
@@ -0,0 +1,35 @@
+# import modules
+
+
+
+
+# data preprocessing
+# 
+
+
+
+
+# configuration
+
+
+
+
+# training data
+
+
+
+
+
+# validation
+
+
+
+
+
+# testing and postprocessing
+
+
+
+
+
+# accuracy metrics and tensorboard
